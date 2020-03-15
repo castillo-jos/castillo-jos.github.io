708ac1517eff77247b3016019ad6bd392911a05a
Jose G. Castillo <Gus@Mac-Owner-1s-MacBook-Pro.local>
2020-03-15 09:06:01 -0400

    Nother Commit

--- publishconf.py
+++ publishconf.py
@@ -11,8 +11,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://castillo-jos.github.io/'
-RELATIVE_URLS = True
+SITEURL = 'https://castillo-jos.github.io/index.html'
+RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
